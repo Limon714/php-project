17619fedff86c4340d5c4a35a1d8e26ddc84e381
Limon Islam <limon4u714@gmail.com>
2023-10-19 00:42:15 +0600

    slug url added

--- ecom/backend/functions.php
+++ ecom/backend/functions.php
@@ -8,13 +8,21 @@ if(isset($_POST['add-category'])){
    $meta_key=$_POST['keyword'];
    $meta_desc=$_POST['description'];
    $added_on=date('M-d-Y');
+   $slug_url = slugUrl($cate_name);
 
-   $sql = "INSERT INTO fv_categories(cate_id,cate_name,meta_title,meta_key,meta_desc,slug_url,added_on,status) VALUES ('$cate_id','$cate_name','$meta_title', '$meta_key','$meta_desc','$cate_name','$added_on',1)";
+   $sql = "INSERT INTO fv_categories(cate_id,cate_name,meta_title,meta_key,meta_desc,slug_url,added_on,status) VALUES ('$cate_id','$cate_name','$meta_title', '$meta_key','$meta_desc','$slug_url','$added_on',1)";
 
    $check = mysqli_query($con,$sql);
    if($check){
     echo "Data Inserted";
    }
 
+}
+
+function slugUrl($string){
+    $slug = preg_replace('/[^a-zA-Z0-9 -]/','',$string);
+    $slug = str_replace(' ','-',$slug);
+    $slug = strtolower($slug);
+    return $slug;
 }
 ?>
