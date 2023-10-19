47fe081c30a55508495727104cb8c223fd3f8274
Limon Islam <limon4u714@gmail.com>
2023-10-19 23:24:22 +0600

    Data Selected and view in admin panel

--- ecom/backend/functions.php
+++ ecom/backend/functions.php
@@ -14,7 +14,12 @@ if(isset($_POST['add-category'])){
 
    $check = mysqli_query($con,$sql);
    if($check){
-    echo "Data Inserted";
+    ?>
+    <script type="text/javascript">
+        alert('Data Inserted Successfully');
+        window.location.href="view-categories.php";
+    </script>
+    <?php 
    }
 
 }
@@ -24,5 +29,22 @@ function slugUrl($string){
     $slug = str_replace(' ','-',$slug);
     $slug = strtolower($slug);
     return $slug;
+}
+
+function get_categories(){
+    include 'db-con.php';
+    $sql = "SELECT * FROM fv_categories ORDER BY id DESC";
+    $check = mysqli_query($con,$sql);
+    while($result = mysqli_fetch_assoc($check)){
+       $output = "<tr>
+       <td> " . $result['cate_id'] .";</td>
+       <td> " . $result['cate_name'] .";</td>
+       <td> " . $result['slug_url'] .";</td>
+       <td> " . $result['added_on'] .";</td>
+       <td> " . $result['status'] .";</td>
+       </tr>" ;
+
+    }
+    return $output;
 }
 ?>
--- ecom/backend/view-categories.php
+++ ecom/backend/view-categories.php
@@ -1,3 +1,4 @@
+<?php include "functions.php"; ?>
 <!DOCTYPE html>
 <html lang="zxx">
 
@@ -156,7 +157,44 @@
             <div class="container-fluid p-0 sm_padding_15px">
                 <div class="row justify-content-center">
                     <div class="col-lg-12 bg-danger">
-                       View Categories
+                    <div class="white_card card_height_100 mb_30">
+                            <div class="white_card_header">
+                                <div class="box_header m-0">
+                                    <div class="main-title">
+                                        <h3 class="m-0">Data table</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="white_card_body">
+                                <div class="QA_section">
+                                    <div class="white_box_tittle list_header">
+                                        <h4>Table</h4>
+                                        <div class="box_right d-flex lms_block">
+                                            <div class="add_button ms-2">
+                                                <a href="#" data-bs-toggle="modal"
+                                                    data-bs-target="#addcategory" class="btn_1">Add New</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="QA_table mb_30">
+                                        <table class="table lms_table_active ">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">Category ID</th>
+                                                    <th scope="col">Category Name</th>
+                                                    <th scope="col">Slug URL</th>
+                                                    <th scope="col">Added On</th>
+                                                    <th scope="col">Status</th>
+                                                </tr>
+                                            </thead>
+                                          <tbody>
+                                            <?php echo get_categories(); ?>
+                                          </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
